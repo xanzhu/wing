e204d5c77bc6b26f5091bac193ffd48d7938462d
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-03-18 16:42:04 +1300

    test: Cache response

--- components/siteMaintenance.vue
+++ components/siteMaintenance.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="text-light-200 font-sans mt-50 text-center">
       <div class="flex items-center justify-center">
-          <h1 class="text-5xl font-bold m-0">XANZHU</h1>
+          <h1 class="text-5xl font-bold m-0">XANZHU SECURITY</h1>
       </div>
       <div class="font-thin text-xl">
-         Upgrading website security..
+         Upgrading website security...
       </div>
     </div>
 </template>
